b137a98456920af9aed710c053e28147894dda41
DavidGuben <Davidjg6@gmail.com>
2018-04-30 20:22:23 -0400

    cleared errors, started featured camera div

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -7,6 +7,13 @@ class Intro extends Component {
         return (
             <View>
                 <div className="intro z-depth-1 animated fadeIn">
+                <div className="featured-camera" style={{width: '1000px', height: '1000px', padding: '50px', float: 'right'}}>
+                    <img 
+                     src={require('./../Images/sony-camera.jpg')}
+                     alt="featured camera"
+                     className="col-md-6"
+                    />
+                </div>
                 <Mask overlay="" className="flex-center" />
                 </div>
             </View>

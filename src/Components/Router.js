ee0614405b298f9bd3d15b256e68bd9ea0c2427f
DavidGuben <Davidjg6@gmail.com>
2018-05-10 11:15:46 -0400

    removed modal from Router

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,14 +3,13 @@ import {Switch, Route} from 'react-router-dom'
 
 import RecentPosts from './RecentPosts'
 import TestPost from './TestPost'
-import Modal from './Modal'
 
 const Router = () => (
     <main>
         <Switch>
             <Route exact path='/' component={RecentPosts} />
             <Route exact path='/Post' component={TestPost}/>
-            <Route exact path='/Modal' component={Modal} />
+            
         </Switch>
     </main> 
 )

eadbf16b55ea4148858101109993f39509b5244f
DavidGuben <Davidjg6@gmail.com>
2018-05-10 10:57:26 -0400

    combined cards and modal into reusable components with class based component props

--- src/Components/PostCard.js
+++ src/Components/PostCard.js
@@ -1,9 +1,37 @@
 import React, {Component} from 'react'
-import {Card,View,CardBody,CardText,CardTitle,CardImage,Button} from 'mdbreact'
+import {Card,View,CardBody,CardText,CardTitle,CardImage,Button,Container,Modal,ModalHeader,ModalBody,ModalFooter} from 'mdbreact'
 
 class PostCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          modal: false
+        };
+    
+        this.toggle = this.toggle.bind(this);
+        this.toggle1 = this.toggle1.bind(this);
+        this.toggle2 = this.toggle2.bind(this);
+        }
+    
+        toggle() {
+            this.setState({
+            modal: !this.state.modal
+            });
+        }
+        toggle1() {
+            this.setState({
+            modal1: !this.state.modal1
+          });
+        }
+        toggle2() {
+            this.setState({
+            modal2: !this.state.modal2
+            });
+        }
+
     render() {
         return (
+            <div>
             <Card>
             <View zoom>
                 <CardImage className="img-fluid" 
@@ -17,6 +45,27 @@ class PostCard extends Component {
                 </Button>
             </CardBody>
             </Card>
+
+            <Container>
+            <Modal isOpen={this.state.modal} toggle={this.toggle} className="animated slideInDown">
+                <ModalHeader toggle={this.toggle}>
+                    {this.props.modalHeader}
+                </ModalHeader>
+                <ModalBody>
+                    {this.props.modalContent}
+                </ModalBody>
+                <ModalFooter>
+                <Button color="secondary" onClick={this.toggle}>
+                    {this.props.secondaryButton}
+                </Button>
+                {' '}
+                <Button color="primary">
+                    {this.props.primaryButton}
+                </Button>
+                </ModalFooter>
+            </Modal>
+            </Container>
+            </div>        
         )
     }
 }
--- src/Components/RecentPosts.js
+++ src/Components/RecentPosts.js
@@ -4,34 +4,6 @@ import PostCard from './PostCard'
 
 
 class RecentPosts extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          modal: false
-        };
-    
-        this.toggle = this.toggle.bind(this);
-        this.toggle1 = this.toggle1.bind(this);
-        this.toggle2 = this.toggle2.bind(this);
-        }
-    
-        toggle() {
-            this.setState({
-            modal: !this.state.modal
-            });
-        }
-        toggle1() {
-            this.setState({
-            modal1: !this.state.modal1
-          });
-        }
-        toggle2() {
-            this.setState({
-            modal2: !this.state.modal2
-            });
-        }  
-          
-
     render() {
         return (
             <Container className="wow fadeIn">
@@ -44,76 +16,46 @@ class RecentPosts extends Component {
                 </div>
 
                 <div className="row">
-                
-                <div className="col-md-4 mb-5">
                 {/*First Post*/}
-                <PostCard
-                 image={require('./../Images/italy-arena.jpg')}
-                 heading="Italy"
-                 description="Some quick example text to build on the card title and make up the bulk of the card's content."
-                 buttonText="Read More" />
-
-                {/*First Post Modal*/}
-                <Container>
-                    <Modal isOpen={this.state.modal} toggle={this.toggle} className="animated slideInDown">
-                        <ModalHeader toggle={this.toggle}>Post title</ModalHeader>
-                        <ModalBody>
-                            Post Content
-                        </ModalBody>
-                        <ModalFooter>
-                        <Button color="secondary" onClick={this.toggle}>Close</Button>{' '}
-                        <Button color="primary">Save changes</Button>
-                        </ModalFooter>
-                    </Modal>
-                </Container>
-
-                </div>
-                
                 <div className="col-md-4 mb-5">
+                    <PostCard
+                    image={require('./../Images/italy-arena.jpg')}
+                    heading="Italy"
+                    description="Some quick example text to build on the card title and make up the bulk of the card's content."
+                    buttonText="Read More" 
+                    modalHeader="First Post"
+                    modalContent="First Post Content."
+                    primaryButton="Share"
+                    secondaryButton="Close"
+                    />
+                </div>
+
                 {/*Second Post*/}
-                <PostCard
-                 image={require('./../Images/italy-arena.jpg')}
-                 heading="Penn"
-                 description="Some quick example text to build on the card title and make up the bulk of the card's content."
-                 buttonText="Read More" />
-
-                {/*Second Post Modal*/}
-                <Container>
-                    <Modal isOpen={this.state.modal1} toggle={this.toggle1} className="animated slideInDown">
-                        <ModalHeader toggle={this.toggle1}>Second Post title</ModalHeader>
-                        <ModalBody>
-                            Post Content
-                        </ModalBody>
-                        <ModalFooter>
-                        <Button color="secondary" onClick={this.toggle1}>Close</Button>{' '}
-                        <Button color="primary">Save changes</Button>
-                        </ModalFooter>
-                    </Modal>
-                </Container>
-
+                <div className="col-md-4 mb-5">
+                    <PostCard
+                    image={require('./../Images/italy-arena.jpg')}
+                    heading="Penn"
+                    description="Some quick example text to build on the card title and make up the bulk of the card's content."
+                    buttonText="Read More"
+                    modalHeader="Second Post"
+                    modalContent="Second Post Content."
+                    primaryButton="Share"
+                    secondaryButton="Close"
+                    />
                 </div>
 
-                <div className="col-md-4 mb-5">
                 {/*Third Post*/}
-                <PostCard
-                 image={require('./../Images/italy-boat.jpg')}
-                 heading="Italy"
-                 description="Some quick example text to build on the card title and make up the bulk of the card's content."
-                 buttonText="Read More" />
-
-                {/*Third Post Modal*/}
-                <Container>
-                    <Modal isOpen={this.state.modal2} toggle={this.toggle2} className="animated slideInDown">
-                        <ModalHeader toggle={this.toggle2}>Third Post title</ModalHeader>
-                        <ModalBody>
-                            Post Content
-                        </ModalBody>
-                        <ModalFooter>
-                        <Button color="secondary" onClick={this.toggle2}>Close</Button>{' '}
-                        <Button color="primary">Save changes</Button>
-                        </ModalFooter>
-                    </Modal>
-                </Container>
+                <div className="col-md-4 mb-5">
+                    <PostCard
+                    image={require('./../Images/italy-boat.jpg')}
+                    heading="Italy"
+                    description="Some quick example text to build on the card title and make up the bulk of the card's content."
+                    buttonText="Read More"
+                    modalHeader="Third Post"
+                    modalContent="Third Post Content."
+                    primaryButton="Share"
+                    secondaryButton="Close"
+                    />
                 </div>
 
                 </div>

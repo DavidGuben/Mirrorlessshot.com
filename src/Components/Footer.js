afa409cc7f142789d4d570c60218b99df5b71f11
DavidGuben <Davidjg6@gmail.com>
2018-05-30 17:16:04 -0400

    formating fix in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,35 +10,39 @@ class Footer extends Component {
                   <div className="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
                     <h6 className="mb-0 white-text">Get connected with us on social media</h6>
                   </div>
+
                   <div className="col-md-6 col-lg-7 text-center text-md-right">
-                    <a className="fb-ic ml-0" href="https://www.facebook.com/MirrorlessShot/" target="_blank">
+                    <a className="fb-ic ml-0" href="https://www.facebook.com/MirrorlessShot/" target="_blank" rel="noopener noreferrer">
                       <i className="fa fa-facebook white-text mr-4"> </i> 
                     </a>
-                    <a className="tw-ic">
+                    <a className="tw-ic" href="/" target="_blank" rel="noopener noreferrer">
                       <i className="fa fa-twitter white-text mr-4"> </i>
                     </a>
-                    <a className="gplus-ic">
+                    <a className="gplus-ic" href="/" target="_blank" rel="noopener noreferrer">
                       <i className="fa fa-google-plus white-text mr-4"> </i>
                     </a>
-                    <a className="li-ic">
+                    <a className="li-ic" href="/" target="_blank" rel="noopener noreferrer">
                       <i className="fa fa-linkedin white-text mr-4"> </i>
                     </a>
-                    <a className="ins-ic">
+                    <a className="ins-ic" href="/" target="_blank" rel="noopener noreferrer">
                        <i className="fa fa-instagram white-text mr-lg-4"> </i>
                     </a>
                    </div>
+
                  </div>
                </div>
              </div>
+
              <div className="container mt-5 mb-4 text-center text-md-left wow fadeIn">
               <div className="row mt-3">
+
                 <div className="col-md-3 col-lg-4 col-xl-3 mb-4">
                   <h6 className="text-uppercase font-weight-bold">
                     <strong>Mirrorless Shot</strong>
                   </h6>
                   <hr className="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" />
                   <p></p>
-                 </div>
+                </div>
 
                 <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
                   <h6 className="text-uppercase font-weight-bold">
@@ -79,7 +83,6 @@ class Footer extends Component {
                 </div>
         
                 <div className="col-md-4 col-lg-3 col-xl-3">
-        
                   <h6 className="text-uppercase font-weight-bold">
                     <strong>Contact</strong>
                   </h6>
@@ -97,15 +100,13 @@ class Footer extends Component {
                     <i className="fa fa-envelope mr-3"></i> 1231 133 1454
                   </p>
                 </div>
-        
+                
               </div>
             </div>
-        
             <div className="footer-copyright py-3 text-center">
               2018 Copyright: <a href="https://davidguben.com">David G.</a>
             </div>
-        
-          </footer>
+            </footer>
         )
     }
 }
